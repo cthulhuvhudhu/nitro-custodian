7378af4ded725b9882dead2917439b7e562f4a28
Jess Sullenberger <jess.sullenberger@iodigital.com>
2023-11-13 14:05:07 +0100

    Stage Three: Generate hints

--- app/src/main/kotlin/nitro/custodian/Grid.kt
+++ app/src/main/kotlin/nitro/custodian/Grid.kt
@@ -8,9 +8,19 @@ class Grid(private val n: Int) {
 
     init {
         assert(n in 0..SIZE*SIZE) { "Must be fewer nitros than slots" }
+        generate()
+        populate()
+        hints()
+
+    }
+
+    private fun generate() {
         (1..SIZE).forEach { _ ->
             field.add(".".repeat(SIZE).toCharArray().toMutableList())
         }
+    }
+
+    private fun populate() {
         var nitros = 0
         while (nitros < n) {
             nitros += Random.nextInt(SIZE*SIZE-1).run {
@@ -19,6 +29,41 @@ class Grid(private val n: Int) {
                     return@run 1
                 }
                 return@run 0
+            }
+        }
+    }
+
+    private fun hints() {
+        for (x in 0 until SIZE) {
+            for (y in 0 until SIZE) {
+                if (field[x][y] != 'X') {
+                    var bombCt = 0
+                    if (x > 0) {
+                        if (field[x-1][y] == 'X') bombCt++
+                        if (y > 0) {
+                            if (field[x-1][y-1] == 'X') bombCt++
+                        }
+                        if (y < SIZE-1) {
+                            if (field[x-1][y+1] == 'X') bombCt++
+                        }
+                    }
+                    if (x < SIZE-1) {
+                        if (field[x+1][y] == 'X') bombCt++
+                        if (y > 0) {
+                            if (field[x+1][y-1] == 'X') bombCt++
+                        }
+                        if (y < SIZE-1) {
+                            if (field[x+1][y+1] == 'X') bombCt++
+                        }
+                    }
+                    if (y > 0) {
+                        if (field[x][y-1] == 'X') bombCt++
+                    }
+                    if (y < SIZE-1) {
+                        if (field[x][y+1] == 'X') bombCt++
+                    }
+                    field[x][y] = bombCt.digitToChar()
+                }
             }
         }
     }
